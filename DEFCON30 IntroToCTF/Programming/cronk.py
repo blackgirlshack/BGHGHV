3fcd039f86649bf8524a6bd2983ebb309a7e3efa
tani-jadoa <110402593+tani-jadoa@users.noreply.github.com>
2022-08-01 15:21:40 -0400

    Update cronk.py

--- DEFCON30 IntroToCTF/Programming/cronk.py
+++ DEFCON30 IntroToCTF/Programming/cronk.py
@@ -5,8 +5,8 @@ c=open('cronk2.txt', 'r')
 flag=c.read()
 
 for x in flag:
-	os.system('clear')
-	print(x, end="\r")
-	time.sleep(5)
+    os.system('clear')
+    print(x, end="\r")
+    time.sleep(5)
 	
 c.close()	
